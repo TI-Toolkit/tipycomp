64508be43e5e8784b0ac48fe27db650b69fb85f5
Adrien Bertrand <bertrand.adrien@gmail.com>
2026-08-08 17:16:29 +0200

    tipydecomp: improve validation and error handling

--- tipydecomp.py
+++ tipydecomp.py
@@ -1,62 +1,102 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import sys
 import tempfile
 
+
+DISASSEMBLER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mpy-disasm", "mpy-disasm")
+
+
+class FormatError(ValueError):
+    pass
+
+
 def get_menu_pos(data):
+    if not data.startswith(b"PYMP"):
+        raise FormatError("payload does not start with PYMP")
+
     length = 0
     index = 4
     shift = 0
     while True:
+        if index >= len(data):
+            raise FormatError("truncated menu length")
         byte = data[index]
         length |= (byte & 0x7f) << shift
         index += 1
         shift += 7
         if byte & 0x80 == 0:
             break
+        if shift >= 35:
+            raise FormatError("menu length is too large")
+
+    if index >= len(data):
+        raise FormatError("missing PYMP format version")
+    if data[index] != 2:
+        raise FormatError("unsupported PYMP format version {}".format(data[index]))
 
     start = index + 1
     end = start + length
+    if end > len(data):
+        raise FormatError("menu extends beyond the payload")
+    if length == 0 or data[end - 1] != 0:
+        raise FormatError("menu is not NUL-terminated")
+    return start, end
 
-    return (start, end)
 
-def dump_menu(data):
-    start, end = get_menu_pos(data)
-    menu = data[start:end-1]
-    print(str(menu, encoding="utf8"))
-
-def disasm(data, temp_dir):
+def split_payload(data):
     start, end = get_menu_pos(data)
     mpy_data = data[end:]
-    mpy_filename = temp_dir + 'in.mpy'
-    open(mpy_filename, 'wb').write(mpy_data)
-    subprocess.run(["mpy-disasm/mpy-disasm", mpy_filename])
+    if len(mpy_data) < 4 or mpy_data[0] != ord("M"):
+        raise FormatError("payload does not contain .mpy bytecode")
+    return data[start:end - 1], mpy_data
+
+
+def dump_menu(data):
+    menu, _ = split_payload(data)
+    print(str(menu, encoding="utf8"))
+
+
+def disasm(data, temp_dir):
+    _, mpy_data = split_payload(data)
+    mpy_filename = temp_dir + "/in.mpy"
+    with open(mpy_filename, "wb") as output:
+        output.write(mpy_data)
+    subprocess.run([DISASSEMBLER, mpy_filename], check=True)
+
 
 def main(mode, infile):
-    temp_dir = tempfile.mkdtemp(prefix='tipycomp_')
+    if mode not in ("menu", "disasm"):
+        print("Invalid mode " + mode)
+        print('Use "menu" or "disasm"')
+        return 1
 
-    bin_filename = temp_dir + '/in.bin'
+    temp_dir = tempfile.TemporaryDirectory(prefix="tipycomp_")
+    bin_filename = temp_dir.name + "/in.bin"
 
     subprocess.run(["convbin",
                     "-i", infile, "-j", "8x",
                     "-o", bin_filename, "-k", "bin"],
-                    stdout=subprocess.DEVNULL)
+                   check=True, stdout=subprocess.DEVNULL)
 
-    data = open(bin_filename, 'rb').read()
+    with open(bin_filename, "rb") as bin_file:
+        data = bin_file.read()
 
-    if mode == 'menu':
+    if mode == "menu":
         dump_menu(data)
-    elif mode == 'disasm':
-        disasm(data, temp_dir)
     else:
-        print('Invalid mode ' + mode)
-        print('Use "menu" or "disasm"')
-        exit(1)
+        disasm(data, temp_dir.name)
+    return 0
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: ' + __file__ + ' (menu|disasm) infile')
-        exit(1)
-    main(*sys.argv[1:])
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: " + __file__ + " (menu|disasm) infile")
+        sys.exit(1)
+    try:
+        sys.exit(main(*sys.argv[1:]))
+    except (FormatError, OSError, subprocess.CalledProcessError) as error:
+        print("tipydecomp: {}".format(error), file=sys.stderr)
+        sys.exit(1)
